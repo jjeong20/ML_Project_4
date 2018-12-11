bb01b169e5e9aa3b1726fd30e26080c5e573033e
James Corbett <james.h.corbett@gmail.com>
2018-12-11 12:49:29 -0500

    Add files via upload

--- Final.py
+++ Final.py
@@ -11,12 +11,15 @@ import sklearn
 from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+from sklearn import kernel_ridge
 from sklearn import ensemble
 from sklearn import metrics
 from sklearn import multioutput
 
-#import sys
-
+# Comment/uncomment the following 3 lines if you're getting annoyed with warnings from sklearn
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
 
 
@@ -37,58 +40,67 @@ class DataBuilder:
     """
 
     def __init__(self, train_path=None, test_path=None, graph_path=None, out_path=None, lift_degree=1, use_mode=True, \
-        mode_param=0.088, replace_missing_values=True, use_lon_as_feature=False, exclude_null_island=True):
+        mode_param=0.088, replace_missing_values=True, exclude_null_island=True, include_posts=False, avg_post_hr=-1):
         """
         Builds the data sets.
 
-        @param train_path:  Path to a text file containing the data for the training set, containing entries 
-                            on each line like "Id,Hour1,Hour2,Hour3,Lat,Lon,Posts\n".
-                            The first line should not contain data.
-        @param test_path:   Path to a text file containing the data for the test set, containing entries 
-                            on each line like "Id,Hour1,Hour2,Hour3,Posts\n".
-                            The first line should not contain data.   
-        @param graph_path:  Path to a text file containing the connections between users, formatted like "user_ID_1\tuser_ID_2". 
-                            The data should begin on the first line.                                           
-        
-        Returns:            An object with fields containing the training set and test set
+        :param train_path:                  Path to a text file containing the data for the training set, containing entries 
+                                            on each line like "Id,Hour1,Hour2,Hour3,Lat,Lon,Posts\n".
+                                            The first line should not contain data.
+        :param test_path:                   Path to a text file containing the data for the test set, containing entries 
+                                            on each line like "Id,Hour1,Hour2,Hour3,Posts\n".
+                                            The first line should not contain data.   
+        :param graph_path:                  Path to a text file containing the connections between users, formatted like "user_ID_1\tuser_ID_2". 
+                                            The data should begin on the first line.                                           
+        :param out_path:                    Name of the text file you want to create that contains the results
+        :param lift_degree:                 Degree that you want to lift your data
+        :param use_mode:                    Include the mode in the training and test sets
+        :param mode_param:                  The "size" of units in the mode
+        :param replace_missing_values:      Replace 0's with the average of that feature space
+        :param exclude_null_island:         Exclude points with (lat,lon) == (0,0) 
+        :param include_posts:               Include number of posts as a feature for each data point
+        :param avg_post_hr:                 What you think the average hour that people post on social media. Used for translating post hour to longitude.
+                                            
+        :Returns:            An object with fields containing the training set and test set
         """
 
         self.mode = use_mode
-        self.use_lon_as_feature = use_lon_as_feature
         self.mode_param = mode_param
         self.lift_degree = lift_degree
         self.out_path = out_path
         self.exclude_null_island = exclude_null_island
+        self.avg_post_hr = avg_post_hr
+        self.include_posts = include_posts
+        self.replace_missing_values = replace_missing_values
         self.graph = self.read_graph(graph_path)
         self.raw_train_data = self.read_in_data(train_path)
         self.user_locations = self.set_locations()
         self.X_tr, self.y_tr_lat, self.y_tr_lon, self.y_both = self.build_training_set()
         self.test_set, self.user_ids = self.build_test_set(test_path)
-        if replace_missing_values is True:
-            self.replace_missing_values_training()
-            self.replace_missing_values_test()
-        #self.print_data(self.X_tr.tolist(), 65)
+        
+        #self.print_data(self.X_tr.tolist(), 10) #65
         #self.print_data(self.test_set.tolist(), 20)
         self.lat_preds = None
         self.lon_preds = None
+        self.both_preds = None
+
 
         self.fit_and_predict()
 
 
 
     def read_graph(self, filename):
+        """
+        Reads connections from the given file and creates a mapping of user_id to that user's connections.
+        """
         friend_graph = {}
-        #count = 0
         with open(filename, "r") as file:
             for line in file:
-                #count+=1
                 data = line.split("\t", 2)
                 user_id = int(data[0])
                 friend_id = int(data[1])
                 if user_id not in friend_graph:
                     friend_graph[user_id] = [friend_id]
-                #elif friend_id in friend_graph[user_id]:
-                    #print("Error in line {0}! This edge between {1} and {2} has already been found.".format(count, user_id, friend_id))   
                 else:
                     friend_graph[user_id].append(friend_id) 
                 if friend_id not in friend_graph:
@@ -131,7 +143,6 @@ class DataBuilder:
 
 
 
-
     def build_training_set(self):
         """
         Builds the training set, which is the pair (X_tr, y_tr).
@@ -154,7 +165,10 @@ class DataBuilder:
                         data_point[i] = 0
                     else:
                         data_point[i] += 1                    
-                X_point = [int(data_point[i]) for i in range(1,4)] + [int(data_point[6])] + self.get_avg_lat_lon(user_id)   #hour1,hour2,hour3,posts,avg_lat,avg_lon in that order
+                X_point = [int(data_point[i]) for i in range(1,4)]
+                if self.include_posts is True:
+                    X_point = X_point + [int(data_point[6])]
+                X_point = X_point + self.get_avg_lat_lon(user_id) 
                 if self.mode is True:
                     X_point = X_point + self.get_mode_lat_lon(user_id, self.mode_param)
                 X_tr.append(X_point)
@@ -163,6 +177,25 @@ class DataBuilder:
                 y_both.append([data_point[4], data_point[5]]) 
             else: 
                 continue    
+        
+        if self.replace_missing_values is True:
+            self.replace_missing_values_data(X_tr)
+        if self.avg_post_hr > -1:
+            for k in range(0, len(X_tr)):
+                hours_to_longitude = [(-15)*(X_tr[k][i]-self.avg_post_hr) for i in range(0,3)]
+                j = 0
+                for x in hours_to_longitude:
+                    if x < -180:
+                        x += 180
+                        hours_to_longitude[j] = -x
+                    elif x >= 180:
+                        x -= 180
+                        hours_to_longitude[j] = -x    
+                    j+=1
+                
+                avg_hour = sum(hours_to_longitude) / len(hours_to_longitude)
+                #X_tr[k] = X_tr[k] + hours_to_longitude
+                X_tr[k].append(avg_hour)
         return (np.array(X_tr), np.array(y_tr_lat), np.array(y_tr_lon), np.array(y_both))
 
     def build_test_set(self, filename):
@@ -183,10 +216,31 @@ class DataBuilder:
                     else:
                         data[i] += 1 
                 users.append(user_id)
-                data_point = [int(data[i]) for i in range(1,5)] + self.get_avg_lat_lon(user_id)
+                data_point = [int(data[i]) for i in range(1,4)] 
+                if self.include_posts is True:
+                    data_point = data_point + [int(data[4])]
+                data_point = data_point + self.get_avg_lat_lon(user_id)
                 if self.mode is True:
                     data_point = data_point + self.get_mode_lat_lon(user_id, self.mode_param)
                 to_return.append(data_point)
+        
+        if self.replace_missing_values is True:
+            self.replace_missing_values_data(to_return)
+        if self.avg_post_hr > -1:
+            for k in range(0, len(to_return)):
+                hours_to_longitude = [(-15)*(to_return[k][i]-self.avg_post_hr) for i in range(0,3)]
+                j = 0
+                for x in hours_to_longitude:
+                    if x < -180:
+                        x += 180
+                        hours_to_longitude[j] = -x
+                    elif x >= 180:
+                        x -= 180
+                        hours_to_longitude[j] = -x    
+                    j+=1
+                
+                avg_hour = sum(hours_to_longitude) / len(hours_to_longitude)
+                to_return[k].append(avg_hour)
         return (np.array(to_return), users) 
 
    
@@ -269,12 +323,19 @@ class DataBuilder:
         """
         f = open(self.out_path, "x")
         f.write("Id,Lat,Lon\n")
-        for user_id, lat_pred, lon_pred in zip(self.user_ids, self.lat_preds, self.lon_preds):
-            f.write("{0},{1},{2}\n".format(user_id, lat_pred, lon_pred))
+        if self.both_preds is not None:
+            for user_id, pair in zip(self.user_ids, self.both_preds):
+                f.write("{0},{1},{2}\n".format(user_id, pair[0], pair[1]))
+        else:
+            for user_id, lat_pred, lon_pred in zip(self.user_ids, self.lat_preds, self.lon_preds):
+                f.write("{0},{1},{2}\n".format(user_id, lat_pred, lon_pred))
         f.close()    
 
 
     def lift_data(self, train_data, test_data):
+        """
+        Lifts data to the degree specified in the __init__() function
+        """
         lifter = sklearn.preprocessing.PolynomialFeatures(degree=self.lift_degree)
 
         lifted_train_data = lifter.fit_transform(train_data)
@@ -282,12 +343,18 @@ class DataBuilder:
         return (lifted_train_data, lifted_test_data)
 
     def standardize_data(self, train_data, test_data):
+        """
+        Uses a StandardScaler to preprocess training and testing data
+        """
         stdizer = sklearn.preprocessing.StandardScaler()
         train_data = stdizer.fit_transform(train_data)
         test_data = stdizer.transform(test_data)
         return (train_data, test_data)
 
     def MinMax_scale_data(self, train_data, test_data):
+        """
+        Uses a MinMaxScaler to preprocess training and testing data
+        """
         scaler = sklearn.preprocessing.MinMaxScaler()
         train_data = stdizer.fit_transform(train_data)
         test_data = stdizer.transform(test_data)
@@ -295,7 +362,13 @@ class DataBuilder:
 
     def cross_val(self, models, X_tr, y_tr, folds=10, scorer="neg_mean_squared_error"):
         """
-        Takes a bunch of models, performs cross validation on them and takes the best model
+        Takes a bunch of models, performs cross validation on them and takes the best model. Prints results. 
+
+        :param models:  A list of models to try
+        :param X_tr:    The set to train the models on (might be lifted data, for instance)
+        :param y_tr:    The target values accompanying y_tr
+        :param folds:   Number of folds for cross-validation
+        :scorer:        The scoring function to use
         """
         max = -np.inf
         best_model = None
@@ -318,74 +391,17 @@ class DataBuilder:
         return best_model        
 
 
-
-    def stack(self, models, top_model, X_train, y_train, y_test):
+    def print_data(self, matrix, count):
         """
-        Takes a list of models and combines them. Top_model learns from them (top_model takes their predictions and learns from them)
-        Returns the predictions. 
+        Prints lines of data matrix. Useful for checking on how yoour features compare to the actual values
 
-
-        NOTE: THIS FUNCTION IS NOT COMPLETE
-
+        :param matrix:  The matrix to print from
+        :param count:   The number of lines to print
         """
-        data = None
-        for model in models:
-                model.fit(X_train, y_train)
-                preds = model.predict(self.test_set).reshape(-1,1).tolist()
-                if data is None:
-                    data = preds
-                else:    
-                    for pt, pred in zip(data, preds):
-                        pt = pt + pred
-
-        data = np.array(data)
-        top_model.fit(data, y_train)
-        return top_model.predict(y_test)
-
-     
-
-    def update_data(self):
-        """
-        Add longitude prediction as a feature
-        """
-
-        #we need the data in list form to do some easy appending
-        train_data = self.X_tr.tolist()
-        longitudes_of_train_set = self.y_tr_lon.reshape(-1, 1).tolist()
-        test_data = self.test_set.tolist()
-        longitude_predictions = self.lon_preds.reshape(-1, 1).tolist()
-
-        if self.lift_degree > 2:
-            lifter = sklearn.preprocessing.PolynomialFeatures(degree=self.lift_degree-2)
-        else:
-            lifter = sklearn.preprocessing.PolynomialFeatures(degree=1)
-        
-        #now lift the longitudes
-        longitudes_of_train_set = lifter.fit_transform(longitudes_of_train_set)
-        longitude_predictions = lifter.transform(longitude_predictions)
-
-        #now scale the longitudes
-        stdizer = sklearn.preprocessing.StandardScaler()
-        stdizer.fit_transform(longitudes_of_train_set)
-        stdizer.transform(longitude_predictions)
-        longitudes_of_train_set = longitudes_of_train_set.tolist()
-        longitude_predictions = longitude_predictions.tolist()
-
-        #add longitudes to training set
-        for train_pt, lon in zip(train_data, longitudes_of_train_set):
-            train_pt = train_pt + lon
-        self.X_tr = np.array(train_data)
-    
-        #add longitude predictions to test set
-        for test_pt, lon_pred in zip(test_data, longitude_predictions):    
-            test_pt = test_pt + lon_pred
-        self.test_set = np.array(test_data)  
-
-
-    def print_data(self, list, count):
         print("hour1,hour2,hour3,posts,avg_lat,avg_lon,\tactual_lat,actual_lon")
         n = 0
-        for line, lat, lon in zip(list, self.y_tr_lat, self.y_tr_lon):
+        print(len(matrix[0]))
+        for line, lat, lon in zip(matrix, self.y_tr_lat, self.y_tr_lon):
             n+=1
             if n is count:
                 return
@@ -393,61 +409,63 @@ class DataBuilder:
                 print(str(line)+",\t"+str(lat)+","+str(lon))        
 
 
-    def replace_missing_values_training(self):
+    def replace_missing_values_data(self, data_matrix):
+        """
+        Replaces points in the data set with value 0 (here representing insufficient information) with the average of that feature space.
+
+        :param data_matrix:     The matrix to perform replacement on
+        """
+        n_features = 5
         if self.mode is True:
-            n_features = 8
-        else:
-            n_features = 6  
+            n_features += 2
+        if self.exclude_null_island is False:
+            n_features += 1   
+
+        n_features = len(data_matrix[0])     
 
         tot = [0 for i in range(n_features)]
         count = [0 for i in range(n_features)]
 
-        for row in self.X_tr:
+        for row in data_matrix:
             for i in range(len(row)):
                 if row[i] != 0:
                     tot[i] += row[i]
                     count[i] += 1
 
-        for row in self.X_tr:
+        for row in data_matrix:
             for i in range(len(row)):
                 if row[i] == 0:
                     row[i] = tot[i]/count[i]
 
-    def replace_missing_values_test(self):
-        if self.mode is True:
-            n_features = 8
-        else:
-            n_features = 6    
 
-        tot = [0 for i in range(n_features)]
-        count = [0 for i in range(n_features)]
-
-        for row in self.test_set:
-            for i in range(len(row)):
-                if row[i] != 0:
-                    tot[i] += row[i]
-                    count[i] += 1
-
-        for row in self.test_set:
-            for i in range(len(row)):
-                if row[i] == 0:
-                    row[i] = tot[i]/count[i]
 
     def fit_and_predict(self):
         """
-        Fit the model and predict.
+        Insert your models here to do the work of predicting on the training set.
         """
-
-
-        #standardize the data
+        
 
         lifted_train_data, lifted_test_data = self.lift_data(self.X_tr, self.test_set)
         lifted_train_data, lifted_test_data = self.standardize_data(lifted_train_data, lifted_test_data)
         self.X_tr, self.test_set = self.standardize_data(self.X_tr, self.test_set)
 
-        """
-        Insert your model here.
-        """
+
+        #Here are some good models, which you might want to use (they're not quite as good as GradientBoostingRegressors)
+        #the models are multi_Ada, multi_ridge_chain, and tree.
+        #All of them predict both latitude and longitude at the same time, rather than one and then the other
+        #Use multioutput.MultiOutputRegressor() or multioutput.RegressorChain() to get multiple predictions at once
+        #Tree models don't need to be fit on lifted data. Only ridge needs to be fit on lifted data.
+        #multi_grad is a GradientBoostingRegressor that does pretty well. Yours might be better, though.
+        ridge = linear_model.Ridge(alpha=2.0, tol=0.0001)
+        multi_ridge_chain = multioutput.RegressorChain(base_estimator=ridge, order=[0,1])
+        tree = sklearn.tree.DecisionTreeRegressor(max_depth=9, min_samples_split=200, min_samples_leaf=100)
+
+        Ada_tree = sklearn.tree.DecisionTreeRegressor(max_depth=2, min_samples_split=100, min_samples_leaf=50)
+        AdaBoost = ensemble.AdaBoostRegressor(base_estimator=Ada_tree, learning_rate=0.02, n_estimators=60)
+        multi_Ada = multioutput.MultiOutputRegressor(estimator=AdaBoost, n_jobs=3)
+
+        gradient_boost = ensemble.GradientBoostingRegressor(learning_rate = 0.065, n_estimators=500)
+        multi_grad = multioutput.MultiOutputRegressor(estimator=gradient_boost, n_jobs=3)
 
         self.print_txt()
         
@@ -455,8 +473,6 @@ class DataBuilder:
 
 
 dataBuilder = DataBuilder(train_path="posts_train.txt", test_path="posts_test.txt", \
-                          graph_path="graph.txt", out_path="CVstuff1.txt", \
-                          lift_degree=4, use_mode=True, mode_param=0.088, replace_missing_values=False, \
-                          use_lon_as_feature=False, exclude_null_island=True)
-#dataBuilder.print_data(10)
-
+                          graph_path="graph.txt", out_path="final2.txt", \
+                          lift_degree=4, use_mode=True, mode_param=0.088, replace_missing_values=True, \
+                          exclude_null_island=False, include_posts=True, avg_post_hr=20)
